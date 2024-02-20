da8ebc715a8b022ae7d79c9926ff07e8b03fc0fb
Spimy <officialspyme@gmail.com>
2024-02-20 23:04:52 +0800

    👌 IMPROVE: Add guild id and config for intro arc

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export default {
-  introChannelId: '1001853076682055830'
+  guildId: '452472873802530817',
+  introRoleId: '1209502990311100426',
+  introChannelId: '1001853076682055830',
+  kickTimeout: 60000
 };

82b10e4a20bd95df2213e1d39de7183ac10f72f7
Spimy <officialspyme@gmail.com>
2024-02-20 22:08:31 +0800

    👌 IMPROVE: Add new env config keys

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,22 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       TOKEN: string;
+      GOOGLE_TYPE: string;
+      GOOGLE_PROJECT_ID: string;
+      GOOGLE_PRIVATE_KEY_ID: string;
+      GOOGLE_PRIVATE_KEY: string;
+      GOOGLE_CLIENT_EMAIL: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_AUTH_URI: string;
+      GOOGLE_TOKEN_URI: string;
+      GOOGLE_AUTH_PROVIDER_CERT_URL: string;
+      GOOGLE_CLIENT_CERT_URL: string;
+      GOOGLE_UNIVERSE_DOMAIN: string;
+      REGISTRATION_FORM_ID: string;
+      EMAIL_HOST: string;
+      EMAIL_PORT: string;
+      EMAIL_USER: string;
+      EMAIL_PASSWORD: string;
     }
   }
 }

b0e1c71b9509d342ae2125e4b264e7c0834a503b
Spimy <officialspyme@gmail.com>
2024-04-11 17:55:20 +0400

    📦 NEW: Add more config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 export default {
   guildId: '452472873802530817',
   introRoleId: '1209502990311100426',
+  freshieRoleId: '1227966875040743434',
+  memberRoleId: '519495094521430076',
   introChannelId: '1001853076682055830',
+  verificationChannelId: '1227956718235947008',
   kickTimeout: 60000,
   spreadsheetColumns: 'F:J'
 };

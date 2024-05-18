febdfbbacb2f9736d01a2b4371b2fef64ac43777
Spimy <officialspyme@gmail.com>
2024-05-19 00:40:49 +0800

    👌 IMPROVE: Push example config instead

---
+++ src/config.example.ts
@@ -0,0 +1,17 @@
+export default {
+  guildId: '',
+
+  roleIds: {
+    intro: '',
+    freshie: '',
+    member: ''
+  },
+
+  channelIds: {
+    intro: '',
+    verification: ''
+  },
+
+  kickTimeout: 60000,
+  spreadsheetColumns: 'F:J'
+};

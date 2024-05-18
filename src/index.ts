8b55aae719ab6eb2184dbab28a37eed8c26e1246
Spimy <officialspyme@gmail.com>
2024-05-18 17:20:10 +0800

    👌 IMPROVE: Create and login to client inside a bootstrap function

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import { KuuClient } from './lib/client';
 // Load environment variables
 dotenv.config();
 
-// Create the discord bot client
-const client = new KuuClient({
-  intents: [
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.DirectMessages
-  ]
-});
+// Setup the bot client and register slash commands
+const bootstrap = async () => {
+  // Create the discord bot client
+  const client = new KuuClient({
+    intents: [
+      IntentsBitField.Flags.GuildMembers,
+      IntentsBitField.Flags.MessageContent,
+      IntentsBitField.Flags.GuildMessages,
+      IntentsBitField.Flags.Guilds,
+      IntentsBitField.Flags.DirectMessages
+    ]
+  });
 
-client.login(process.env.TOKEN);
+  client.login(process.env.TOKEN);
+};
+
+bootstrap();

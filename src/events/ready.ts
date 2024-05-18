1d9a0bafca54f98cdeb9e9cff15efadddc9c99ea
Spimy <officialspyme@gmail.com>
2024-05-18 17:18:39 +0800

    📦 NEW: Add method to register slash commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,18 @@
-import { Client } from 'discord.js';
+import { Client, REST, Routes } from 'discord.js';
 import config from '../config';
 import { EventListener } from '../lib/abstract/events';
+import { KuuClient } from '../lib/client';
 import { kickControl } from '../lib/utils';
 
 export default class extends EventListener<'ready'> {
-  constructor() {
+  constructor(private client: KuuClient) {
     super('ready');
   }
 
   async execute(client: Client<true>): Promise<void> {
+    // Register commands
+    await this.registerCommands();
+
     client.guilds.fetch(config.guildId).then(async (guild) => {
       // Fetch all members in the guild
       await guild.members.fetch();
@@ -26,5 +30,28 @@ export default class extends EventListener<'ready'> {
 
     // Notify success for discord bot login
     console.log(`Logged in as ${client.user.username}`);
+  }
+
+  private async registerCommands() {
+    // Create the rest client to register slash commands
+    const rest = new REST().setToken(process.env.TOKEN);
+
+    // Register the slash commands
+    await rest
+      .put(Routes.applicationGuildCommands(process.env.APPLICATION_ID, config.guildId), {
+        body: this.client.slashCommands.map((cmd) => cmd.info)
+      })
+      .then((cmds) => {
+        console.log(
+          `Successfully registered ${(cmds as unknown[]).length} commands in guild '${
+            config.guildId
+          }'.`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Something went wrong while registering commands for guild '${config.guildId}': ${error}`
+        );
+      });
   }
 }
